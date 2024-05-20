df41c2d24857e1188d17a522af26dbc864dc6639
godLei6 <603785348@qq.com>
2024-05-20 14:59:08 +0800

    feat: 完善项目

--- websocket/userSubscribe.go
+++ websocket/userSubscribe.go
@@ -1,92 +1,80 @@
 package websocket
 
-//type UserSubscribe struct {
-//	store kv.Store
-//}
+import (
+	"context"
+	"fmt"
+	"gitee.com/i-Things/share/ctxs"
+	"gitee.com/i-Things/share/eventBus"
+	"gitee.com/i-Things/share/utils"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/stores/kv"
+	"sync"
+	"time"
+)
 
-//func NewUserSubscribe(store kv.Store) *UserSubscribe {
-//	return &UserSubscribe{
-//		store: store,
-//	}
-//}
-//
-//func (u *UserSubscribe) genUserKey(userID int64) string {
-//	return fmt.Sprintf("cache:subcribe:user:%v", userID)
-//}
-//
-//func (u *UserSubscribe) genInfoKey(info *SubscribeInfo) string {
-//	params, _ := json.Marshal(info.Params)
-//	return fmt.Sprintf("cache:subcribe:info:%v:%v", info.Code, utils.MD5V(params))
-//}
-//
-//func (u *UserSubscribe) Add(ctx context.Context, userID int64, info *SubscribeInfo) error {
-//	if dp.connPool[userID] == nil {
-//		return errors.Parameter.AddMsg("ws未连接")
-//	}
-//	nodeStr := cast.ToString(nodeID)
-//	err := u.store.HsetCtx(ctx, u.genInfoKey(info), cast.ToString(userID), nodeStr)
-//	if err != nil {
-//		return err
-//	}
-//	_, err = u.store.HsetnxCtx(ctx, u.genUserKey(userID), utils.MarshalNoErr(info), nodeStr)
-//	return err
-//}
-//
-//func (u *UserSubscribe) Del(ctx context.Context, userID int64, info *SubscribeInfo) error {
-//	u.store.HdelCtx(ctx, u.genInfoKey(info), cast.ToString(userID))
-//	_, err := u.store.HdelCtx(ctx, u.genUserKey(userID), utils.MarshalNoErr(info))
-//	return err
-//}
-//
-//func (u *UserSubscribe) Clear(ctx context.Context, userID int64) error {
-//	infos, err := u.store.HkeysCtx(ctx, u.genUserKey(userID))
-//	if err != nil {
-//		return err
-//	}
-//	for _, v := range infos {
-//		var info SubscribeInfo
-//		json.Unmarshal([]byte(v), &info)
-//		_, err := u.store.HdelCtx(ctx, u.genInfoKey(&info), cast.ToString(userID))
-//		if err != nil {
-//			logx.WithContext(ctx).Error(err)
-//		}
-//	}
-//	_, err = u.store.DelCtx(ctx, u.genUserKey(userID))
-//	return err
-//}
-//
-//// 返回的key是nodeid,value是userIDs
-//func (u *UserSubscribe) IndexInfo(ctx context.Context, info *SubscribeInfo) (map[int64][]int64, error) {
-//	val, err := u.store.HgetallCtx(ctx, u.genInfoKey(info))
-//	if err != nil {
-//		return nil, err
-//	}
-//	var ret = map[int64][]int64{}
-//	for kStr, vStr := range val {
-//		k := cast.ToInt64(kStr)
-//		v := cast.ToInt64(vStr)
-//		if ret[v] == nil {
-//			ret[v] = []int64{k}
-//			continue
-//		}
-//		ret[v] = append(ret[v], k)
-//	}
-//	return ret, nil
-//}
-//
-//func (u *UserSubscribe) Index(ctx context.Context, userID int64) ([]*SubscribeInfo, error) {
-//	members, err := u.store.HkeysCtx(ctx, u.genUserKey(userID))
-//	if err != nil {
-//		return nil, err
-//	}
-//	var ret []*SubscribeInfo
-//	for _, v := range members {
-//		var val SubscribeInfo
-//		err := json.Unmarshal([]byte(v), &val)
-//		if err != nil {
-//			continue
-//		}
-//		ret = append(ret, &val)
-//	}
-//	return ret, nil
-//}
+const (
+	asyncExecMax = 500
+)
+
+type publishStu struct {
+	*WsPublish
+	ctx context.Context
+}
+
+type UserSubscribe struct {
+	publishChan chan publishStu //key是apisvr的节点id
+	mutex       sync.RWMutex
+	ServerMsg   *eventBus.FastEvent
+}
+
+func NewUserSubscribe(store kv.Store, ServerMsg *eventBus.FastEvent) *UserSubscribe {
+	u := UserSubscribe{publishChan: make(chan publishStu, asyncExecMax), ServerMsg: ServerMsg}
+	utils.Go(context.Background(), func() {
+		u.publish()
+	})
+	return &u
+}
+
+func (u *UserSubscribe) Publish(ctx context.Context, code string, data any, params ...map[string]any) error {
+	pb := WsPublish{
+		Code: code,
+		Data: data,
+	}
+	for _, param := range params {
+		pb.Params = append(pb.Params, utils.Md5Map(param))
+	}
+	u.publishChan <- publishStu{
+		WsPublish: &pb,
+		ctx:       ctxs.CopyCtx(ctx),
+	}
+	logx.Infof("UserSubscribe.publish:%v", utils.Fmt(pb))
+
+	return nil
+}
+
+func (u *UserSubscribe) publish() {
+	execCache := make([]publishStu, 0, asyncExecMax)
+	exec := func() {
+		if len(execCache) == 0 {
+			return
+		}
+		logx.Infof("UserSubscribe.publish publishs:%v", utils.Fmt(execCache))
+		err := u.ServerMsg.Publish(context.Background(), fmt.Sprintf(eventBus.CoreApiUserPublish, 1), execCache)
+		if err != nil {
+			logx.Error(err)
+		}
+		execCache = execCache[0:0] //清空切片
+	}
+	tick := time.Tick(time.Second)
+	for {
+		select {
+		case _ = <-tick:
+			exec()
+		case e := <-u.publishChan:
+			execCache = append(execCache, e)
+			if len(execCache) > asyncExecMax {
+				exec()
+			}
+		}
+	}
+}
