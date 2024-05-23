4410d9a90d403018f0416200f286c4d95260abbf
godLei6 <603785348@qq.com>
2024-05-23 11:03:55 +0800

    feat: 完善项目

--- errors/device.go
+++ errors/device.go
@@ -3,10 +3,11 @@ package errors
 const DeviceError = 2000000
 
 var (
-	RespParam      = NewCodeError(DeviceError+1, "返回参数不对")
-	DeviceTimeOut  = NewCodeError(DeviceError+2, "设备回复超时")
-	NotOnline      = NewCodeError(DeviceError+3, "设备不在线")
-	DeviceResp     = NewCodeError(DeviceError+4, "设备回复错误")
-	DeviceBound    = NewCodeError(DeviceError+5, "设备已被绑定")
-	DeviceNotBound = NewCodeError(DeviceError+6, "设备未绑定")
+	RespParam       = NewCodeError(DeviceError+1, "返回参数不对")
+	DeviceTimeOut   = NewCodeError(DeviceError+2, "设备回复超时")
+	NotOnline       = NewCodeError(DeviceError+3, "设备不在线")
+	DeviceResp      = NewCodeError(DeviceError+4, "设备回复错误")
+	DeviceBound     = NewCodeError(DeviceError+5, "设备已被绑定")
+	DeviceNotBound  = NewCodeError(DeviceError+6, "设备未绑定")
+	DeviceCantBound = NewCodeError(DeviceError+7, "设备无法绑定")
 )
