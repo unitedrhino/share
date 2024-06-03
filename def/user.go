5091ef2b868635531dff831642cc16b7bb460d35
godLei6 <603785348@qq.com>
2024-06-03 14:02:50 +0800

    feat: 完善项目

--- def/user.go
+++ def/user.go
@@ -10,3 +10,11 @@ const (
 	AccountTypeWechatOpen   = "wechatOpen"
 	AccountTypeDingTalkUser = "dingTalkUser"
 )
+
+type RoleCode = string
+
+const (
+	RoleCodeDistributor RoleCode = "distributor" //经销商
+	RoleCodeAdmin       RoleCode = "admin"       //管理员
+	RoleCodeSupper      RoleCode = "supper"      //超级管理员(平台管理员)
+)
