9bc2f789a67d386bb166da9f911cdb554a8426e3
godLei6 <603785348@qq.com>
2024-06-13 13:57:00 +0800

    feat: 完善项目

--- clients/dingTalk.go
+++ clients/dingTalk.go
@@ -1,8 +1,11 @@
 package clients
 
 import (
+	"encoding/json"
 	"gitee.com/i-Things/share/conf"
 	"gitee.com/i-Things/share/errors"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
 	"github.com/zhaoyunxing92/dingtalk/v2"
 )
 
@@ -19,109 +22,40 @@ func NewDingTalkClient(c *conf.ThirdConf) (*DingTalk, error) {
 	return cli, nil
 }
 
-//
-//
-//
-//
-//
-///**
-// * 使用 Token 初始化账号Client
-// * @return Client
-// * @throws Exception
-// */
-//func CreateAliClient() (_result *dingtalkoauth2_1_0.Client, _err error) {
-//	config := &openapi.Config{}
-//	config.Protocol = tea.String("https")
-//	config.RegionId = tea.String("central")
-//	_result = &dingtalkoauth2_1_0.Client{}
-//	_result, _err = dingtalkoauth2_1_0.NewClient(config)
-//	return _result, _err
-//}
-//
-//func _main(args []*string) (_err error) {
-//	client, _err := CreateAliClient()
-//	if _err != nil {
-//		return _err
-//	}
-//
-//	getAccessTokenRequest := &dingtalkoauth2_1_0.GetAccessTokenRequest{
-//		AppKey:    tea.String("dingu3zh1aifodxsgtye"),
-//		AppSecret: tea.String("MLPuoBoeMdko2xVFLgkrfLYe_GU_nk0urFx1fPgV_heCgAIh0VXzikxWa43zGpIM"),
-//	}
-//	tryErr := func() (_e error) {
-//		defer func() {
-//			if r := tea.Recover(recover()); r != nil {
-//				_e = r
-//			}
-//		}()
-//		_, _err = client.GetAccessToken(getAccessTokenRequest)
-//		if _err != nil {
-//			return _err
-//		}
-//
-//		return nil
-//	}()
-//
-//	if tryErr != nil {
-//		var err = &tea.SDKError{}
-//		if _t, ok := tryErr.(*tea.SDKError); ok {
-//			err = _t
-//		} else {
-//			err.Message = tea.String(tryErr.Error())
-//		}
-//		if !tea.BoolValue(util.Empty(err.Code)) && !tea.BoolValue(util.Empty(err.Message)) {
-//			// err 中含有 code 和 message 属性，可帮助开发定位问题
-//		}
-//
-//	}
-//	return _err
-//}
-//
-//func _main2(args []*string) (_err error) {
-//	client, _err := CreateAliClient()
-//	if _err != nil {
-//		return _err
-//	}
-//
-//	getUserTokenRequest := &dingtalkoauth2_1_0.GetUserTokenRequest{
-//		ClientSecret: tea.String("wgea"),
-//		ClientId:     tea.String("erqFDGS"),
-//		Code:         tea.String("afae"),
-//		GrantType:    tea.String("gwegweg"),
-//		RefreshToken: tea.String("asdfe"),
-//	}
-//	tryErr := func() (_e error) {
-//		defer func() {
-//			if r := tea.Recover(recover()); r != nil {
-//				_e = r
-//			}
-//		}()
-//		_, _err = client.GetUserToken(getUserTokenRequest)
-//		if _err != nil {
-//			return _err
-//		}
-//
-//		return nil
-//	}()
-//
-//	if tryErr != nil {
-//		var err = &tea.SDKError{}
-//		if _t, ok := tryErr.(*tea.SDKError); ok {
-//			err = _t
-//		} else {
-//			err.Message = tea.String(tryErr.Error())
-//		}
-//		if !tea.BoolValue(util.Empty(err.Code)) && !tea.BoolValue(util.Empty(err.Message)) {
-//			// err 中含有 code 和 message 属性，可帮助开发定位问题
-//		}
-//
-//	}
-//	return _err
-//}
-//
-//func main() {
-//	err := _main(tea.StringSlice(os.Args[1:]))
-//	if err != nil {
-//		panic(err)
-//	}
-//}
+type DingRobot = dingtalk.Robot
+
+func NewDingRobotClient(token string) DingRobot {
+	return dingtalk.NewRobot(token)
+}
+
+// 钉钉消息结构体
+type text struct {
+	Content string `json:"content" validate:"required"`
+}
+
+// 文本消息
+type textMessage struct {
+	MsgType string `json:"msgtype" validate:"required,oneof=text image voice file link oa markdown action_card feedCard"`
+	text    `json:"text" validate:"required"`
+}
+
+func (t *textMessage) Validate(valid *validator.Validate, trans ut.Translator) error {
+	return nil
+}
+
+func (t *textMessage) String() string {
+	str, _ := json.Marshal(t)
+	return string(str)
+}
+
+func (t *textMessage) MessageType() string {
+	return "text"
+}
+
+// NewTextMessage 文本对象
+func NewTextMessage(context string) *textMessage {
+	msg := &textMessage{}
+	msg.MsgType = msg.MessageType()
+	msg.text = text{Content: context}
+	return msg
+}
