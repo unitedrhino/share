ea608b92cfb4f778b215289d5c150113aef8145d
godLei6 <603785348@qq.com>
2024-07-18 16:37:27 +0800

    feat: 完善代码

--- websocket/subscribe.go
+++ websocket/subscribe.go
@@ -11,14 +11,14 @@ func subscribeHandle(ctx context.Context, c *connection, body WsReq) {
 	var info SubscribeInfo
 	err := mapstructure.Decode(body.Body, &info)
 	if err != nil {
-		logx.Error(err)
+		logx.Errorf("userSubscribe Decode body:%v err:%v", utils.Fmt(body), err)
 		c.errorSend(err)
 		return
 	}
 	if checkSubscribe != nil {
 		err = checkSubscribe(ctx, &info)
 		if err != nil {
-			logx.Error(err)
+			logx.Errorf("userSubscribe checkSubscribe body:%v err:%v", utils.Fmt(body), err)
 			c.errorSend(err)
 			return
 		}
