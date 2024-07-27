c7abb49c3ae1a574de9b2162d5f2c6bf6156ca00
godLei6 <603785348@qq.com>
2024-07-27 15:27:22 +0800

    feat: 完善项目

--- def/userSubscribe.go
+++ def/userSubscribe.go
@@ -4,6 +4,7 @@ type UserSubscribe = string
 
 var (
 	UserSubscribeDevicePropertyReport = "devicePropertyReport"
+	UserSubscribeDeviceActionReport   = "deviceActionReport"
 	UserSubscribeDeviceConn           = "deviceConn"
 	UserSubscribeDeviceOtaReport      = "deviceOtaReport"
 	UserSubscribeUserOrderPay         = "userOrderPay"
