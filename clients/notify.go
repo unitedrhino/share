dcb0a9976dba6dec6d72016b47ee1bec3046da15
godLei6 <603785348@qq.com>
2024-08-12 18:07:05 +0800

    feat: 完善项目

--- clients/notify.go
+++ clients/notify.go
@@ -1,7 +1,10 @@
 package clients
 
 import (
+	"context"
+	"fmt"
 	"gitee.com/i-Things/share/utils"
+	"github.com/zeromicro/go-zero/core/proc"
 	"os"
 )
 
@@ -12,14 +15,20 @@ func init() {
 		utils.SetPanicNotify(func(s string) {
 			c.SendRobotMsg(NewTextMessage("抓到panic:" + s))
 		})
+		proc.AddShutdownListener(func() {
+			e, _ := os.Executable()
+			c.SendRobotMsg(NewTextMessage(fmt.Sprintf("iThings程序退出:%v", e)))
+		})
 	}
 }
 
 func SysNotify(in string) {
 	token := os.Getenv("dingRobotToken")
 	if token != "" {
-		c := NewDingRobotClient(token)
-		c.SendRobotMsg(NewTextMessage("系统通知:" + in))
-
+		ctx := context.Background()
+		utils.Go(ctx, func() {
+			c := NewDingRobotClient(token)
+			c.SendRobotMsg(NewTextMessage("iThings系统通知:" + in))
+		})
 	}
 }
