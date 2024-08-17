2d6d2dded0935cdae1472d16961d51e4127741db
godLei6 <603785348@qq.com>
2024-08-17 15:55:16 +0800

    feat: 完善项目

--- errors/device.go
+++ errors/device.go
@@ -5,7 +5,7 @@ const DeviceError = 2000000
 var (
 	RespParam       = NewCodeError(DeviceError+1, "返回参数不对")
 	DeviceTimeOut   = NewCodeError(DeviceError+2, "设备回复超时")
-	NotOnline       = NewCodeError(DeviceError+3, "设备不在线")
+	NotOnline       = NewCodeError(DeviceError+3, "设备离线，请检查电源或设备")
 	DeviceResp      = NewCodeError(DeviceError+4, "设备回复错误")
 	DeviceBound     = NewCodeError(DeviceError+5, "设备已被绑定")
 	DeviceNotBound  = NewCodeError(DeviceError+6, "设备未绑定")
