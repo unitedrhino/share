c651a1a32772c15a33e4cda9df3883941e27198e
godLei6 <603785348@qq.com>
2024-08-28 14:19:39 +0800

    feat: 完善项目`

--- systems/notify.go
+++ systems/notify.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"gitee.com/i-Things/share/clients/dingClient"
 	"gitee.com/i-Things/share/utils"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/proc"
 	"os"
+	"runtime"
+	"runtime/debug"
 )
 
 func init() {
@@ -19,6 +22,14 @@ func init() {
 		proc.AddShutdownListener(func() {
 			e, _ := os.Executable()
 			c.SendRobotMsg(dingClient.NewTextMessage(fmt.Sprintf("iThings程序退出:%v", e)))
+		})
+	} else {
+		proc.AddShutdownListener(func() {
+			pc := make([]uintptr, 1)
+			runtime.Callers(3, pc)
+			f := runtime.FuncForPC(pc[0])
+			msg := fmt.Sprintf("Shutdown|func=%s|stack=%s\n", f, string(debug.Stack()))
+			logx.Error(msg)
 		})
 	}
 }
