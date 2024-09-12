ce51c93b5f9d7fa2628fa4b75ff0a99baf118827
godLei6 <603785348@qq.com>
2024-09-12 10:14:44 +0800

    feat: 完善项目`

--- def/user.go
+++ def/user.go
@@ -15,6 +15,7 @@ type RoleCode = string
 
 const (
 	RoleCodeDistributor RoleCode = "distributor" //经销商
+	RoleCodeClient      RoleCode = "client"      //c端普通用户
 	RoleCodeAdmin       RoleCode = "admin"       //管理员
 	RoleCodeSupper      RoleCode = "supper"      //超级管理员(平台管理员)
 )
