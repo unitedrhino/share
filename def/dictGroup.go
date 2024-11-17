276d5310853996e8f69ba0980aa51240d21be57d
godLei6 <603785348@qq.com>
2024-11-17 11:54:05 +0800

    feat: 更新定义

--- def/dictGroup.go
+++ def/dictGroup.go
@@ -3,7 +3,7 @@ package def
 type DictGroup = string
 
 var (
-	DictGroupBase   DictGroup = "base"   //基础配置
-	DictGroupThing  DictGroup = "thing"  //物联网
-	DictGroupSystem DictGroup = "system" //系统管理
+	DictGroupBase   DictGroup = "base"         //基础配置
+	DictGroupThings DictGroup = "things"       //物联网
+	DictGroupSystem DictGroup = "systemManage" //系统管理
 )
