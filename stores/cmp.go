ca474e0eb9270be32cdd58f10a2ab33ccf0db20b
godLei6 <603785348@qq.com>
2025-01-13 21:04:32 +0800

    feat: 数据库过滤函数新增json的查询

--- stores/cmp.go
+++ stores/cmp.go
@@ -2,6 +2,7 @@ package stores
 
 import (
 	"fmt"
+	"gitee.com/unitedrhino/share/utils"
 	"gorm.io/gorm"
 	"strings"
 )
@@ -22,7 +23,7 @@ const (
 	CmpTypeLike  CmpType = "like"   //模糊查询
 )
 
-type toSqlFunc func(column string) string
+type toSqlFunc func(db *DB, column string) string
 
 type Cmp struct {
 	Value     any
@@ -30,7 +31,7 @@ type Cmp struct {
 }
 
 func defaultToSql(c CmpType) toSqlFunc {
-	return func(column string) string {
+	return func(db *DB, column string) string {
 		return fmt.Sprintf("%s %s ?", column, string(c))
 	}
 }
@@ -71,7 +72,7 @@ func CmpLt(value any) *Cmp {
 
 // 是否是null
 func CmpIsNull(isNull bool) *Cmp {
-	return &Cmp{toSqlFunc: func(column string) string {
+	return &Cmp{toSqlFunc: func(db *DB, column string) string {
 		var isNullStr = "null"
 		if isNull == false {
 			isNullStr = "not null"
@@ -81,13 +82,13 @@ func CmpIsNull(isNull bool) *Cmp {
 }
 
 func CmpEqColumn(c CmpType, columnLeft string) *Cmp {
-	return &Cmp{toSqlFunc: func(column string) string {
+	return &Cmp{toSqlFunc: func(db *DB, column string) string {
 		return fmt.Sprintf("%s %s %s", columnLeft, c, column)
 	}}
 }
 
 func CmpLike(value string) *Cmp {
-	return &Cmp{toSqlFunc: func(column string) string {
+	return &Cmp{toSqlFunc: func(db *DB, column string) string {
 		return fmt.Sprintf("%s like %s", column, "%"+value+"%")
 	}}
 }
@@ -110,9 +111,34 @@ func CmpNotIn[t any](values ...t) *Cmp {
 	return &Cmp{toSqlFunc: defaultToSql(CmpTypeNotIn), Value: values}
 }
 
+// json对象中 obj.key = v
+func CmpJsonObjEq(k string, v any) *Cmp {
+	return &Cmp{toSqlFunc: func(db *DB, column string) string {
+		return fmt.Sprintf("JSON_CONTAINS(%s, JSON_OBJECT(?,?))", column)
+	}, Value: []any{k, v}}
+}
+
+// json对象中 obj.key like '%v%'
+func CmpJsonObjLike(k string, v any) *Cmp {
+	return &Cmp{toSqlFunc: func(db *DB, column string) string {
+		return fmt.Sprintf("JSON_SEARCH(%s, 'all', '%s', NULL, '$.\"%s\"') IS NOT NULL", column)
+	}, Value: []any{k, "%" + utils.ToString(v) + "%"}}
+}
+
+// json 数组中包含 v的值 或方式
+func CmpJsonArrayOrHas[t any](v ...t) *Cmp {
+	return &Cmp{toSqlFunc: func(db *DB, column string) string {
+		var s []string
+		for range v {
+			s = append(s, fmt.Sprintf("JSON_CONTAINS(%s, ?)", column))
+		}
+		return strings.Join(s, " or ")
+	}, Value: utils.ToAnySlice(v)}
+}
+
 // 过滤二进制比特位
 func CmpBinEq(bit int64, isHigh int64) *Cmp {
-	return &Cmp{toSqlFunc: func(column string) string {
+	return &Cmp{toSqlFunc: func(db *DB, column string) string {
 		return fmt.Sprintf("(%s >> %d) & 1 = ?", column, bit)
 	}, Value: isHigh}
 }
@@ -122,10 +148,10 @@ func CmpAnd(cs ...*Cmp) *Cmp {
 	for _, v := range cs {
 		values = append(values, v.ToValues()...)
 	}
-	return &Cmp{Value: values, toSqlFunc: func(column string) string {
+	return &Cmp{Value: values, toSqlFunc: func(db *DB, column string) string {
 		var sqls []string
 		for _, v := range cs {
-			sqls = append(sqls, v.ToSql(column))
+			sqls = append(sqls, v.ToSql(db, column))
 		}
 		return strings.Join(sqls, " and ")
 	}}
@@ -136,10 +162,10 @@ func CmpOr(cs ...*Cmp) *Cmp {
 	for _, v := range cs {
 		values = append(values, v.ToValues()...)
 	}
-	return &Cmp{Value: values, toSqlFunc: func(column string) string {
+	return &Cmp{Value: values, toSqlFunc: func(db *DB, column string) string {
 		var sqls []string
 		for _, v := range cs {
-			sqls = append(sqls, v.ToSql(column))
+			sqls = append(sqls, v.ToSql(db, column))
 		}
 		return strings.Join(sqls, " or ")
 	}}
@@ -149,7 +175,7 @@ func CmpOr(cs ...*Cmp) *Cmp {
 func CmpBtw(max any, min any) *Cmp {
 	return &Cmp{
 		Value: []any{max, min},
-		toSqlFunc: func(column string) string {
+		toSqlFunc: func(db *DB, column string) string {
 			return fmt.Sprintf("%s <= ? && %s >= ?", column, column)
 		},
 	}
@@ -169,16 +195,16 @@ func (g *Cmp) ToValues() []any {
 	}
 }
 
-func (g *Cmp) ToSql(column string) string {
+func (g *Cmp) ToSql(db *DB, column string) string {
 	if g == nil {
 		return ""
 	}
-	return g.toSqlFunc(column)
+	return g.toSqlFunc(db, column)
 }
 
-func (g *Cmp) Where(db *gorm.DB, column string) *gorm.DB {
+func (g *Cmp) Where(db *DB, column string) *gorm.DB {
 	if g != nil {
-		db = db.Where(g.ToSql(column), g.ToValues()...)
+		db = db.Where(g.ToSql(db, column), g.ToValues()...)
 	}
 	return db
 }
