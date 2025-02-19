99a8640ff0347088b3e809d45b54cceddb5265c9
godLei6 <603785348@qq.com>
2025-02-19 22:55:32 +0800

    feat: 用户更新

--- users/login.go
+++ users/login.go
@@ -14,21 +14,25 @@ type LoginClaims struct {
 }
 
 type UserInfo struct {
-	UserID     int64  `json:",string"`
-	Account    string //账号
-	RoleIDs    []int64
-	RoleCodes  []string
-	TenantCode string `json:",string"`
-	IsAdmin    int64
-	IsAllData  int64
+	UserID      int64 `json:",string"`
+	LastTokenID string
+	Account     string //账号
+	RoleIDs     []int64
+	RoleCodes   []string
+	TenantCode  string `json:",string"`
+	IsAdmin     int64
+	IsAllData   int64
 }
 
-func GetLoginJwtToken(secretKey string, userID int64, appCode string, id string) (string, error) {
+func GetLoginJwtToken(secretKey string, t time.Time, seconds int64, userID int64, appCode string, id string) (string, error) {
+	IssuedAt := jwt.NewNumericDate(t)
 	claims := LoginClaims{
 		UserID:  userID,
 		AppCode: appCode,
 		RegisteredClaims: jwt.RegisteredClaims{
-			ID: id,
+			ExpiresAt: jwt.NewNumericDate(t.Add(time.Duration(seconds) * time.Second)),
+			IssuedAt:  IssuedAt,
+			ID:        id,
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
