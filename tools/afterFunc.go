1d9bee6b3be370616c2359611a422ae1c7df4292
godLei6 <603785348@qq.com>
2025-12-05 11:42:42 +0800

    feat: 封装数据库执行后执行函数,优化函数逻辑

---
+++ tools/afterFunc.go
@@ -0,0 +1,51 @@
+package tools
+
+import (
+	"context"
+
+	"gitee.com/unitedrhino/share/ctxs"
+	"gitee.com/unitedrhino/share/stores"
+)
+
+type AfterFunc struct {
+	txFuncs []func(db *stores.DB) error //业务处理完之后需要在事务内处理
+	funcs   []func(ctx context.Context) //执行完之后异步处理,事务实行完之后处理就行
+}
+
+func (a *AfterFunc) AddFunc(f func(ctx context.Context)) {
+	a.funcs = append(a.funcs, f)
+}
+
+func (a *AfterFunc) AddTxFunc(f func(db *stores.DB) error) {
+	a.txFuncs = append(a.txFuncs, f)
+}
+
+func (a *AfterFunc) Handle(ctx context.Context, handle func(db *stores.DB) error) error {
+	if a == nil || (len(a.txFuncs) == 0 && len(a.funcs) == 0) {
+		return stores.GetTenantConn(ctx).Transaction(func(tx *stores.DB) error {
+			return handle(tx)
+		})
+	}
+	err := stores.GetTenantConn(ctx).Transaction(func(tx *stores.DB) error {
+		err := handle(tx)
+		if err != nil {
+			return err
+		}
+		for _, f := range a.txFuncs {
+			err := f(tx)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	for _, f := range a.funcs {
+		ctxs.GoNewCtx(ctx, func(ctx context.Context) {
+			f(ctx)
+		})
+	}
+	return nil
+}
